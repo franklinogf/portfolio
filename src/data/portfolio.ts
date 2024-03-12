e5b4910fb79bbd490aeac95e772ae6e9e569577d
Franklin Gonzalez <franklinomarflores@gmail.com>
2024-03-12 19:02:52 -0400

    removed back slashes of the projects image url

--- src/data/portfolio.ts
+++ src/data/portfolio.ts
@@ -13,10 +13,10 @@ const portfolio: Portfolio = {
     }
   ],
   projects: [
-    { name: 'Project 1', image_url: `http://fakeimg.pl/800x460?text=Project 1&font=museo` },
-    { name: 'Project 2', image_url: `http://fakeimg.pl/800x460?text=Project 2&font=museo` },
-    { name: 'Project 3', image_url: `http://fakeimg.pl/800x460?text=Project 3&font=museo` },
-    { name: 'Project 4', image_url: `http://fakeimg.pl/800x460?text=Project 4&font=museo` }
+    { name: 'Project 1', image_url: 'http://fakeimg.pl/800x460?text=Project 1&font=museo' },
+    { name: 'Project 2', image_url: 'http://fakeimg.pl/800x460?text=Project 2&font=museo' },
+    { name: 'Project 3', image_url: 'http://fakeimg.pl/800x460?text=Project 3&font=museo' },
+    { name: 'Project 4', image_url: 'http://fakeimg.pl/800x460?text=Project 4&font=museo' }
   ],
   footer_description:
     'Seasoned Full Stack Software Engineer with over 8 years of hands-on experience in designing and implementing robust, scalable, and innovative web solutions. Adept at leveraging a comprehensive skill set encompassing front-end and back-end technologies'
