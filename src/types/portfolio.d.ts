88c7dc9de66894900024403c8e0b3081ee3da90e
Franklin Gonzalez <franklinomarflores@gmail.com>
2024-03-11 22:20:30 -0400

    added a type

---
+++ src/types/portfolio.d.ts
@@ -0,0 +1,22 @@
+type Date4Numbers = `${number}${number}${number}${number}`
+type Date2Numbers = `${number}${number}`
+type Date = `${Date4Numbers}-${Date2Numbers}-${Date2Numbers}`
+
+export interface Experience {
+  name: Capitalize<string>
+  startDate: Date
+  endDate?: Date
+  description: Capitalize<string>
+}
+export interface Project {
+  name: Capitalize<string>
+  image_url: string
+}
+
+export interface Portfolio {
+  name: string
+  title: string
+  description: string
+  projects: Project[]
+  experiences: Experience[]
+}
